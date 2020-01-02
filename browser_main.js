72593f28d838ce95569a31c71eaff4e1b58c3712
Richard Campbell <richard.campbell@tdameritrade.com>
2020-01-02 10:03:10 -0500

    updates to latest dbtools

--- browser_main.js
+++ browser_main.js
@@ -1,17 +1,19 @@
 // is strict via browser <script>
 
-import { BallparksDB } from "./ballparksDB.js"
+import { getBallparksDB } from "./ballparksDB.js"
 import * as dbt from './dbTools.js'
 
+const BallparksDB = getBallparksDB()
 const ndb = dbt.normalize(BallparksDB)
 const ndbp = dbt.countp(ndb)
-const visited4 = dbt.oFilter( ndbp, (o,p) => o[p]==4 ? p : null )
+const visitedBy4 = (o,p) => o[p]==4 ? p : null
+const bparksVisitedByAll = dbt.oFilter( ndbp, visitedBy4 )
 
 const pEls = ndb.map( o => ({el:document.getElementById(o.p),n:o.n}) ).filter(pel=>pel.el)
 pEls.forEach( pel => {
       pel.el.innerHTML += `<span class='${pel.n}'> ${pel.n} </span>`
 })
 
-const v4Els = visited4.map( p => document.getElementById(p) ).filter(x=>x)
+const v4Els = bparksVisitedByAll.map( p => document.getElementById(p) ).filter(x=>x)
 v4Els.forEach( el => el.classList.add("allFamily") )
 
